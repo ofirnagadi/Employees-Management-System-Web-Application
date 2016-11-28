d4092c4d84b01d75e006e03a7753c61ccd4fb89a
GalZaken <Gal.zaken@gmail.com>
2016-11-28 18:26:03 +0200

    package.json file fixed2

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-var express = require('express');
-var app = express();
-
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-
-app.listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+server.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
